462880e7f8cb1f404fd413fe4f39d0a7883f90cb
pkage <pkage@mit.edu>
2015-11-16 13:48:20 -0500

    made ocr work

--- ocr.py
+++ ocr.py
@@ -2,8 +2,19 @@
 
 from photochop.photochop import Photochopper
 from cnn import network, receptor
-import argparse, uuid, os
+import argparse, uuid, os, time, enchant
+from multiprocessing import Pool, cpu_count
+from functools import partial
 
+
+
+def mp(arr):
+    rec = receptor.Receptor();
+    rec.setInputArr(arr);
+    rec.generateReceptors();
+    rec.setCharacter("x");
+    values = rec.getOutput();
+    return values;
 
 if __name__=="__main__":
     # setting up the argument parser
@@ -12,10 +23,14 @@ if __name__=="__main__":
     parser.add_argument('--auto-align', action='store_true', required=False, help="auto align the input document");
     parser.add_argument('--pre-smooth', action='store_true', required=False, help="pre-smooth the input document");
     parser.add_argument('--minimum-group-size', type=int, required=False, help="set the minimum group size to be accepted");
-    parser.add_argument('--set-threshold-to', type=int, required=False, help="set the threshold for a match (0-255)");
+    parser.add_argument('--set-threshold-to', type=int, required=False, help="set the threshold for a match (0-255)", default=200);
     parser.add_argument('--enable-multiprocessing', action='store_true', default=False, help="Multi-processing, default false");
     parser.add_argument('--read-weights', type=str, required=True, help="weight information set to use");
+    parser.add_argument('--spellcheck', action='store_true', required=False, help='spellcheck everything');
     opts = parser.parse_args();
+
+    if opts.spellcheck:
+        chk = enchant.Dict('en_US');
 
     # initialize the argument parser
     dicer = Photochopper(opts.filename, opts.set_threshold_to);
@@ -33,9 +48,9 @@ if __name__=="__main__":
     dicer.process();
     dicer.process_words();
 
-    # shoehorned api - *somebody* doesn't have nice docs, or wrappers, or anything else for that matter
-    activedir = str(uuid.uuid4());
-    dicer.write_out(activedir);
+    print('initializing neural network...');
+    net = network.Network(26, 150, 7);
+    net.importWeights(os.path.join('cnn', 'weights', opts.read_weights, 'wi.csv'), os.path.join('cnn', 'weights', opts.read_weights, 'wo.csv'));
 
     if opts.enable_multiprocessing:
         print("Receptor : Starting Multiprocessing...");
@@ -48,8 +63,34 @@ if __name__=="__main__":
             print('Error getting core counts, setting threadcount to 2');
         threads = Pool(threadcount);
 
-        for x in xrange(1,10):
-            imgs.append([arr]); # PATRICK THIS IS WHERE TO INPUT ARR
+        doc = [];
+        
+        for key in dicer.words:
+            line = dicer.words[key];
+            linedata = "";
+            
+            for i in range(0, len(line)):
+                word = line[i];
+                imgs = [];
+                for img in word:
+                    imgs.append(img);
+                final = threads.map(partial(mp), imgs);
+                net.setTestingData(final);
+                net.outlist = [];
+                net.recognize();
+                wrd = ''.join(net.outlist);
+                if opts.spellcheck and not chk.check(wrd) and (i + 1 != len(line)) and i != 0:
+                    sgg = chk.suggest(wrd); 
+                    wrd = sgg[0] if len(sgg) > 0 else wrd;
+                linedata += wrd + " "; 
+                
+            doc.append(linedata);
+
+        print('output:\n' + '\n'.join(doc));
+                    
+
+                
+            
 
         print("Distributing tasks across " + str(threadcount) + " cores...");
         final = threads.map(partial(mp), imgs);
@@ -57,10 +98,3 @@ if __name__=="__main__":
         end = time.time();
         print("Time Elapsed: " + str(end - start) + " seconds");
 
-    def mp((arr)):
-        receptor = Receptor();
-        receptor.setInputArr(arr);
-        receptor.generateReceptors();
-        receptor.setCharacter("x");
-        values = receptor.getOutput();
-        return values;
